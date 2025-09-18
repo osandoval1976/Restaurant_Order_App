623b579eec6b2c08d4b7baec01e6e2d314c1397d
Racso <oscarhs0576@gmail.com>
2025-09-18 10:57:22 -0400

    updated function

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import {menuArray} from './data.js'
-
 const renderHtml = document.getElementById('newHTML')
 const newElem = ''
-function renderHTML(item){
-const newArray = item.map((items) =>{
+function arrayHTML(){
+    
+return menuArray.forEach((items) =>{
 newElem = `
 <div>
 <span>${items.name}</span>
 <p>${items.ingredients}</p>
 <P>${items.id}</p>
 <p>${items.emoji}</p>
-
 </div>`
 
 
 
 }).join('')
-renderHTML.innerHTML += `<p>${newArray}</p>`
+
 }
 
-console.log(renderHTML())
+renderHtml.innerHTML += `<p>${arrayHTML()}</p>`
